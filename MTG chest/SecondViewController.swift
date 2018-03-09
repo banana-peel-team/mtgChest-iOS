7f57a5b402cd4fa6160b190b8f89711e8b000afb
Diego Maidana <diegomaidana2@gmail.com>
2018-03-09 17:14:44 -0300

    Initial Commit

---
+++ MTG chest/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  MTG chest
+//
+//  Created by Diego Maidana on 3/9/18.
+//  Copyright © 2018 Diego Maidana. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
